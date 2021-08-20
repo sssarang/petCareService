ec5e7012dde4ef787da78adea252b7d806b8395e
HeeChanCh <goodman7190@gmail.com>
2021-08-20 17:30:35 +0900

    test

---
+++ src/main/java/com/bitcamp/petcare/home/controller/HomeController.java
@@ -0,0 +1,26 @@
+package com.bitcamp.petcare.home.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.NoArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+
+@Log4j2
+@NoArgsConstructor
+
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/")
+	public String home() {
+		log.debug("homepage() invoked.");
+		
+		//hello
+		return "home/home";
+	} //homepage
+	
+} //end class
