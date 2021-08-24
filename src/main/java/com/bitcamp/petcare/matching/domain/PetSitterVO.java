7c55e387e05cd69070dee2f116631c1ee1109089
sssarang <88396112+sssarang@users.noreply.github.com>
2021-08-25 02:30:11 +0900

    mybatis-config 설정추가

---
+++ src/main/java/com/bitcamp/petcare/matching/domain/PetSitterVO.java
@@ -0,0 +1,34 @@
+package com.bitcamp.petcare.matching.domain;
+
+import java.util.Date;
+
+import lombok.Data;
+import lombok.Value;
+
+
+@Value
+public class PetSitterVO extends CommonDTO {
+
+	//TBL_PETSITTER_PROFILE
+	private Integer userNo;
+	private String proPhoto;
+	private String introduce;
+
+	//TBL_SERVICE_TYPE
+	private String serviceTypeCode;
+	private Integer price;
+	
+	//TBL_SERVICE_CALENDAR
+	private Date serviceDate;
+	
+	//TBL_SERVICE_PETKINDS
+	private String petTypeCode;
+
+	//TBL_PETSITTER_SKILL
+	private String skillTypeCode;
+	
+	//TBL_ACTIVITY_PHOTO
+	private Integer phtoNo;
+	private String actPhoto;
+	
+} //end class
